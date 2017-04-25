d3ee7bd643a2a08116abddffc325f3cd6d45c1dc
quartorz <tsuhaso@gmail.com>
2017-04-26 05:54:06 +0900

    v1.1

--- invisiblebutton.cpp
+++ invisiblebutton.cpp
@@ -1,6 +1,7 @@
 #include "invisiblebutton.h"
 
 #include <QPaintEvent>
+#include <QPainter>
 
 InvisibleButton::InvisibleButton(QWidget *parent) :
     QPushButton(parent)
@@ -10,4 +11,9 @@ InvisibleButton::InvisibleButton(QWidget *parent) :
 void InvisibleButton::paintEvent(QPaintEvent *e)
 {
     e->accept();
+
+    if (underMouse()) {
+        QPainter painter(this);
+        painter.fillRect(rect(), QColor(255, 255, 255, 80));
+    }
 }
